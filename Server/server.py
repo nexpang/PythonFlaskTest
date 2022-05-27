72e5108d4e082777a2b0780e34024dc547b3abbc
nexpang <rlarudgur123@gmail.com>
2022-05-27 20:11:15 +0900

    회원가입, 로그인 + jwt 토큰 클라 전달 까지 구현완료

--- Server/server.py
+++ Server/server.py
@@ -1,7 +1,10 @@
 #from distutils.debug import DEBUG
 #from glob import escape
 #from unicodedata import name
+#from datetime import datetime, timedelta
 from flask import Flask, request, jsonify
+import bcrypt
+import jwt
 
 # mysql 라이브러리 임포트: pip install pymysql
 import pymysql
@@ -18,20 +21,7 @@ app = Flask(__name__)
 def index():
     return jsonify(200)
 
-
-@app.route('/calculator/<string:operation>/<int:v1>/<int:v2>')
-def calculator(operation, v1, v2):
-    if operation == "add":
-        ret = v1 + v2
-    elif operation == "sub":
-        ret = v1 - v2
-    elif operation == "mul":
-        ret = v1 * v2
-    elif operation == "div":
-        ret = v1 / v2
-    return f'Operation {operation} with {v1} and {v2} is {ret}'
-
-
+"""
 @app.route('/initData/<string:name>')
 def initData(name):
     conn = pymysql.connect(host='localhost', user=os.environ.get('DBuser'), password=os.environ.get('DBpass'), db='study_db', charset='utf8')
@@ -48,28 +38,7 @@ def initData(name):
         conn.close()
 
     return f'{result}'
-
-@app.route('/getData', methods=['GET', 'POST'])
-def getName():
-    if request.method =="POST":
-        params = request.get_json()
-
-        conn = pymysql.connect(host='localhost', user=os.environ.get('DBuser'), password=os.environ.get('DBpass'), db='study_db', charset='utf8')
-        cur = conn.cursor()
-
-        sql = 'SELECT * FROM users WHERE _id = %s;'
-        vals = params['id']
-        cur.execute(sql, vals)
-
-        rows = cur.fetchall()
-        print(rows)
-
-        cur.close()
-        conn.close()
-        return jsonify(rows)
-    
-    return 1
-
+""" 
 @app.route("/register", methods=['POST'])
 def register_proc():
     params = request.get_json()
@@ -81,38 +50,64 @@ def register_proc():
     conn = pymysql.connect(host='localhost', user=os.environ.get('DBuser'), password=os.environ.get('DBpass'), db='study_db', charset='utf8')
     cur = conn.cursor()
 
-    sql = 'SELECT * FROM users WHERE uId = %s;'
-    cur.execute(sql, u_id)
+    sql = 'SELECT EXISTS(SELECT * FROM users WHERE u_id = %s);'
+    result = cur.execute(sql, u_id)
 
-    rows = cur.fetchone()
-    print(rows)
+    result = cur.fetchone()[0]
+
+    if result > 0:
+        cur.close()
+        conn.close()
+        return jsonify({'result': False, 'str':'이미 같은 아이디가 존재합니다.'})
+
+    sql = 'INSERT INTO users(u_id, u_pass, u_name) VALUES (%s, %s, %s);'
+    encrypted_password = bcrypt.hashpw(u_pw.encode("utf-8"), bcrypt.gensalt())
+    cur.execute(sql, (u_id,  encrypted_password.decode("utf-8"), u_name))
+    
+    conn.commit()
 
     cur.close()
     conn.close()
+    
+    return jsonify({'result': True, 'str':'회원가입 성공'})
 
-    return sql
-
-@app.route("/login", methods=['POST'])
+@app.route("/login", methods=['POST']) 
 def login_proc():
-	input_data = request.form()
-	user_id = input_data['id']
-	user_pw = input_data['pw']
+    params = request.get_json()
+    u_id = params['uId']
+    u_pw = params['uPw']
+    
+    conn = pymysql.connect(host='localhost', user=os.environ.get('DBuser'), password=os.environ.get('DBpass'), db='study_db', charset='utf8')
+    cur = conn.cursor()
 
-	# 아이디, 비밀번호가 일치하는 경우
-	if (user_id == admin_id and
-			user_pw == admin_pw):
-		payload = {
-			'id': user_id,
-			'exp': datetime.utcnow() + timedelta(seconds=60)  # 로그인 24시간 유지
-		}
-		token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
+    sql = 'SELECT id, u_pass FROM users WHERE u_id = %s;'
+    result = cur.execute(sql, u_id)
+    
+    cur.close()
+    conn.close()
+    
+    if(result <= 0):
+        return jsonify({'result':False, 'str':'해당하는 아이디가 존재하지 않습니다.'})
+    
 
-		return jsonify({'result': 'success', 'token': token})
+    rows = cur.fetchall()
+    row = rows[0]
+    id = row[0]
+    admin_pw = row[1]
 
-
+    # 아이디, 비밀번호가 일치하는 경우
+    if (bcrypt.checkpw(u_pw.encode('utf-8'), admin_pw.encode('utf-8'))):
+        payload = {
+            'id' : id,
+            #'exp' : datetime.utcnow + timedelta(seconds=60)
+        }
+        token = jwt.encode(payload, os.environ.get('SECRET_KEY'), algorithm='HS256')
+        return jsonify({'result': True,'str':'로그인 성공' ,'token': token})
 	# 아이디, 비밀번호가 일치하지 않는 경우
-	else:
-		return jsonify({'result': 'fail'})
+    else:
+        return jsonify({'result': False, 'str':'비밀번호가 일치하지 않습니다.'})
+    
+
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000)
--- Unity/PythonFlaskTest/Assets/Scripts/WebManager.cs
+++ Unity/PythonFlaskTest/Assets/Scripts/WebManager.cs
@@ -12,11 +12,33 @@ public class User
     public string uName;
 }
 
+[System.Serializable]
+public class Result
+{
+    public bool result;
+    public string str;
+    public string token;
+}
+
 public class WebManager : MonoBehaviour
 {
-    public InputField postData;
+    public CanvasGroup loginPanel;
+    public InputField loginIdInputField;
+    public InputField loginPwInputField;
 
-    const string url = "http://127.0.0.1:5000/getData";
+    public CanvasGroup registerPanel;
+    public InputField registerNameInputField;
+    public InputField registerIdInputField;
+    public InputField registerPwInputField;
+    public InputField registerPwCInputField;
+
+    public CanvasGroup popUpPanel;
+    public Text popUpText;
+
+    private bool getRusult = false;
+    private string token = null;
+
+    const string URL = "http://127.0.0.1:5000";
 
     private void Start()
     {
@@ -24,14 +46,9 @@ public class WebManager : MonoBehaviour
         //StartCoroutine(Upload(url));
     }
 
-    public void PostSend()
+    IEnumerator RegisterPost(string json)
     {
-        string json = JsonUtility.ToJson(new User() { uId = "wlrn98765", uPw = "chfhrdl365!", uName = "nexpang" });
-        StartCoroutine(Upload(url));
-    }
-
-    IEnumerator Upload(string url, string json)
-    {
+        string url = URL + "/register";
         using (UnityWebRequest www = UnityWebRequest.Post(url, json))
         {
             byte[] jsonToSend = System.Text.Encoding.UTF8.GetBytes(json);
@@ -48,6 +65,16 @@ public class WebManager : MonoBehaviour
                 case UnityWebRequest.Result.Success:
                     Debug.Log("Form upload complete!");
                     Debug.Log(www.downloadHandler.text);
+                    Result result = JsonUtility.FromJson<Result>(www.downloadHandler.text);
+                    if(result.result)
+                    {
+                        OpenPopup(result.str);
+                        OpenLoginPanel();
+                    }
+                    else
+                    {
+                        OpenPopup(result.str);
+                    }
                     break;
                 case UnityWebRequest.Result.ConnectionError:
                 case UnityWebRequest.Result.ProtocolError:
@@ -57,6 +84,107 @@ public class WebManager : MonoBehaviour
             }
         }
     }
+
+
+    IEnumerator LoginPost(string json)
+    {
+        string url = URL + "/login";
+        using (UnityWebRequest www = UnityWebRequest.Post(url, json))
+        {
+            byte[] jsonToSend = System.Text.Encoding.UTF8.GetBytes(json);
+            www.uploadHandler = new UploadHandlerRaw(jsonToSend);
+            www.downloadHandler = new DownloadHandlerBuffer();
+            www.SetRequestHeader("Content-Type", "application/json");
+
+            yield return www.SendWebRequest();
+
+            switch (www.result)
+            {
+                case UnityWebRequest.Result.InProgress:
+                    break;
+                case UnityWebRequest.Result.Success:
+                    Debug.Log("Form upload complete!");
+                    Debug.Log(www.downloadHandler.text);
+                    Result result = JsonUtility.FromJson<Result>(www.downloadHandler.text);
+                    if (result.result)
+                    {
+                        token = result.token;
+                        OpenPopup(result.str);
+                    }
+                    else
+                    {
+                        OpenPopup(result.str);
+                    }
+                    break;
+                case UnityWebRequest.Result.ConnectionError:
+                case UnityWebRequest.Result.ProtocolError:
+                case UnityWebRequest.Result.DataProcessingError:
+                    Debug.LogError(www.error);
+                    break;
+            }
+        }
+    }
+
+    public void OnClickRegister()
+    {
+        if(registerNameInputField.text == "" ||registerIdInputField.text == "" || registerPwInputField.text == ""|| registerPwCInputField.text == "")
+        {
+            OpenPopup("비어있는 항목이 있습니다");
+            return;
+        }
+        else if(registerPwInputField.text != registerPwCInputField.text)
+        {
+            OpenPopup("비밀번호가 일치하지 않습니다");
+            return;
+        }
+        string json = JsonUtility.ToJson(new User() { uId = registerIdInputField.text, uPw = registerPwInputField.text, uName = registerNameInputField.text });
+        StartCoroutine(RegisterPost(json));
+    }
+
+    public void OnClickLogin()
+    {
+        if (loginIdInputField.text == "" || loginPwInputField.text == "")
+        {
+            OpenPopup("비어있는 항목이 있습니다");
+            return;
+        }
+        string json = JsonUtility.ToJson(new User() { uId = loginIdInputField.text, uPw = loginPwInputField.text});
+        StartCoroutine(LoginPost(json));
+    }
+
+    private void OpenPopup(string popUpString)
+    {
+        popUpText.text = popUpString;
+        OpenPanel(popUpPanel, true);
+    }
+
+    public void ClosePopup()
+    {
+        popUpText.text = "";
+        OpenPanel(popUpPanel, false);
+    }
+
+
+    public void OpenLoginPanel()
+    {
+        OpenPanel(registerPanel, false);
+        OpenPanel(loginPanel, true);
+    }
+
+    public void OpenResisterPanel()
+    {
+        OpenPanel(loginPanel, false);
+        OpenPanel(registerPanel, true);
+    }
+
+    private void OpenPanel(CanvasGroup cvsGroup, bool enable)
+    {
+        cvsGroup.alpha = enable ? 1 : 0;
+        cvsGroup.interactable = enable;
+        cvsGroup.blocksRaycasts = enable;
+    }
+
+
 
     IEnumerator GetRequest(string url)
     {
